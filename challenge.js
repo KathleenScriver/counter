64936be30362a3cab94645f2fb10524f9809e56e
KathleenScriver <klescriver@gmail.com>
2019-09-05 15:07:14 -0600

    Adds user add, remove functions.

--- challenge.js
+++ challenge.js
@@ -1,13 +1,24 @@
 const counter = document.getElementById('counter');
+const upSecond = document.getElementById('+');
+const downSecond = document.getElementById('-');
 
-myTimer = () => {
+addSecond = () => {
   currentSecond = (document.getElementById('counter').innerText) * 1;
   newSecond = currentSecond += 1;
   counter.innerText = newSecond.toString();
 }
 
+removeSecond = () => {
+  currentSecond = (document.getElementById('counter').innerText) * 1;
+  newSecond = currentSecond -= 1;
+  counter.innerText = newSecond.toString();
+}
+
+upSecond.addEventListener('click', addSecond);
+downSecond.addEventListener('click', removeSecond);
+
 startTimer = () => {
-  setInterval(myTimer, 1000);
+  setInterval(addSecond, 1000);
 }
 
 startTimer();
